1c593672f652250ba80aed02dc29a08756e786d1
Thibaut <tibeloute@msn.com>
2022-08-21 23:53:21 +0200

    utilisation de , goal here récupérer un chiffre du html,dans le ts: l'attribut de la méthode récupère l'event, on récupère le chiffre, de là, on peut afficher le pokémon en fonction du chiffre inscrit dans l'input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list'; // list
-import { Pokemon } from './pokemon'; // class
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +9,21 @@ import { Pokemon } from './pokemon'; // class
 
 export class AppComponent implements OnInit {
 
-  pokemonList: Pokemon[] = POKEMONS;  // import from mock-pokemon-list  
+  pokemonList: Pokemon[] = POKEMONS;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[5]);
   }
 
-  selectPokemon(pokemon: Pokemon) {  // l'import de la class pokemon permet d'utiliser le typage ici
-    console.log(`vous avez cloqué sur le pokémon ${pokemon.name}`);
-  }  // de ce fait, on peut utiliser les propriétés de la class pokémon
+  selectPokemon(event: MouseEvent) {  //1.2 en vue de l'utilisation de $event dans le html
+    // l'event ici ▲ ne suffit pas, il est nécessaire d'utiliser le MouseEvent 
+    const index: number = +(event.target as HTMLInputElement).value;
+    // 1.3 index: number car on souhaite récupérer un chiffre ( voir html)
+    // event.target pour utilser le event, cependant il faut le casté, d'ou le 'as HTMLInputElement'  
+    // Afin de récupérer la valeur, on fini avec le .value, cependant , il return un String
+    // le +   permet de passer le type string en type number.
+    console.log(`vous avez cloqué sur le pokémon ${this.pokemonList[index].name}`);
+    // si on ne met pas de chiffre, la valeur null devient 0 ( systeme JS), on aura donc bulbizarre ( car pokemonList[0].name >> bulbizarre)
+  }
 
 }
