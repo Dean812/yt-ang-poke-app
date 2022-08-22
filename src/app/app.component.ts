a96ab1b0a2bcdb70b08c4453b6dcee89979a8b83
Thibaut <tibeloute@msn.com>
2022-08-23 00:51:11 +0200

    Utilisation du #dièse + interpolation, l'input est directement placé dans le paramètres de la fonction dans l'input, l'interpollation renvoie le nom du pokémon recupéré par la dièse.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,28 @@ import { Pokemon } from './pokemon';
 
 export class AppComponent implements OnInit {
 
-  pokemonList: Pokemon[] = POKEMONS;
+  pokemonList: Pokemon[] = POKEMONS; // récupération de la liste pokémon dans le mock
+  pokemonSelected: Pokemon | undefined; // attribut de type Pokemon
 
   ngOnInit() {
     console.table(this.pokemonList);
   }
 
-  // selectPokemon(event: MouseEvent) {
-  //   const index: number = +(event.target as HTMLInputElement).value;
-  //   console.log(`vous avez cloqué sur le pokémon ${this.pokemonList[index].name}`);
-  // }
+  selectPokemon(pokemonId: string) { // input.value retourne un    String, malgré le input number
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemonOuAutre => pokemonOuAutre.id == +pokemonId);
+    // plutot que de faire des index +1 pour égalisé les chiffres, on recherche une égalité        ▲ 
+    // pokemonouautre c'ets pour dire qu'on peut mettre ce que l'on veut comme nom
+    // si on ne place pas le pokemon/undifined, la valeur pokemonSelected ne prend pas, il faut ajouter a ca 
+    // la condition if / else sont obligatoire pour gérer le undifined
+    // function (pekomonOuAutre) { pokemonOuAutre.id == +pokemonId }
 
+    if (pokemon) {   // si Pokemon
+      console.log(`vous avez cloqué sur le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon; 
+    } else {    // si undifined
+      console.log(`vous avez cloqué sur un pokémon qui n'existe pas`);
+      //this.pokemonSelected = pokemon;       <-- pas nécessaire
+    }
+
+  }
 }
