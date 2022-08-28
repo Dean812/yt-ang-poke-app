a7f20b8a1cd5013ed86cc8e3eb947e88075fc0e4
Thibaut <tibeloute@msn.com>
2022-08-28 13:16:29 +0200

    Passage du keyup en keyup.enter, les keyup sont des pseudo evenement d'angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,20 +16,15 @@ export class AppComponent implements OnInit {
     console.table(this.pokemonList);
   }
 
-  selectPokemon(pokemonId: string) { // input.value retourne un    String, malgré le input number
-    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemonOuAutre => pokemonOuAutre.id == +pokemonId);
-    // plutot que de faire des index +1 pour égalisé les chiffres, on recherche une égalité        ▲ 
-    // pokemonouautre c'ets pour dire qu'on peut mettre ce que l'on veut comme nom
-    // si on ne place pas le pokemon/undifined, la valeur pokemonSelected ne prend pas, il faut ajouter a ca 
-    // la condition if / else sont obligatoire pour gérer le undifined
-    // function (pekomonOuAutre) { pokemonOuAutre.id == +pokemonId }
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(getPokemon => getPokemon.id == +pokemonId);
 
     if (pokemon) {   // si Pokemon
       console.log(`vous avez cloqué sur le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon; 
+      this.pokemonSelected = pokemon;
     } else {    // si undifined
       console.log(`vous avez cloqué sur un pokémon qui n'existe pas`);
-      //this.pokemonSelected = pokemon;       <-- pas nécessaire
+
     }
 
   }
