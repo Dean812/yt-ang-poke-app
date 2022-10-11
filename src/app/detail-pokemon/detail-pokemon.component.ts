a25480a29dd85ccdcfbd951657412f2a2e509e3c
Thibaut <tibeloute@msn.com>
2022-10-12 00:20:45 +0200

    préparation coté TS de la récupération des infos du pokemon au chargement de la page pokemons/id

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
 
@@ -8,11 +9,21 @@ import { Pokemon } from '../pokemon';
 })
 export class DetailPokemonComponent implements OnInit {
 
-  pokemonList: Pokemon[] = POKEMONS;
+  pokemonList: Pokemon[] | undefined; // récupération de la liste de pokemon via la classe pokemon sous forme d'array (propriété) 
+  pokemon: Pokemon | undefined;  // pokemon courant via la class pokemon, les données récupéré rentrerons dedans
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }  //access to router in order to get the ID  ( pokemon/id) , injection dans le composant
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    this.pokemonList = POKEMONS;
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
+    // sapshot : la donnée a l'instant T , paramMap : des paramères  transmit via paramMap ( via un tableau avec tout les paramètres)
+    // le service route renvoie un string     -------------           number ou null 
+    if (pokemonId) {
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id === +pokemonId); // rappel pokemonId est un string , le + le passe en type number
+    } // dans this.pokemon, on rentre dans la liste , on cherche s'il y a correspondance  
+
   }
 
 }
