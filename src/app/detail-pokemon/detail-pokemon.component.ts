8175b59be5038dccd3ec22fbe630cf37ee6b7abc
Thibaut <tibeloute@msn.com>
2022-10-16 23:07:30 +0200

    Gestion affichage de la page du pokemon en fonction de son ID, uniquement l'affichage

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -12,12 +12,12 @@ export class DetailPokemonComponent implements OnInit {
   pokemonList: Pokemon[] | undefined; // récupération de la liste de pokemon via la classe pokemon sous forme d'array (propriété) 
   pokemon: Pokemon | undefined;  // pokemon courant via la class pokemon, les données récupéré rentrerons dedans
 
-  constructor(private route: ActivatedRoute) { }  //access to router in order to get the ID  ( pokemon/id) , injection dans le composant
+  constructor(private route: ActivatedRoute) { }  //access to router in order to get the ID  (pokemon/id) , injection dans le composant, récupère l'id qui est dans l'url.
 
   ngOnInit() {
 
     this.pokemonList = POKEMONS;
-    const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id'); // récupération de l'id
     // sapshot : la donnée a l'instant T , paramMap : des paramères  transmit via paramMap ( via un tableau avec tout les paramètres)
     // le service route renvoie un string     -------------           number ou null 
     if (pokemonId) {
