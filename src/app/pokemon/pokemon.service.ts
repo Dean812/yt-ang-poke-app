4ec670374c965ac9a757e6125e9cef10c1b8b3bb
Thibaut <tibeloute@msn.com>
2022-10-23 15:17:31 +0200

    Mise en place du service pour le module pokemon, préparation des méthodes effectué

---
+++ src/app/pokemon/pokemon.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  getPokemonList(): Pokemon[] {
+    return POKEMONS;
+  }
+
+  getPokemonById(pokemonId: number): Pokemon | undefined {
+    return POKEMONS.find(pokemon => pokemon.id === pokemonId);
+  }
+
+  getPokemonTypeList(): string[] {
+    return [
+      'Plante',
+      'Feu',
+      'Eau',
+      'Insecte',
+      'Normal',
+      'Electrik',
+      'Poison',
+      'Fée',
+      'Vol',
+      'Combat',
+      'Psy'
+    ];
+  }
+
+}
